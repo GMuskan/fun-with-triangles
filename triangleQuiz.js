8a242dc6380d8ea473d36e5a3c9f38281d70461d
GMuskan <55504711+GMuskan@users.noreply.github.com>
2022-07-09 23:31:13 +0530

    feat: triangleQuiz

---
+++ triangleQuiz.js
@@ -0,0 +1,21 @@
+const quizForm = document.querySelector("#quiz-form");
+const btnSubmitAnswers = document.querySelector("#btn-submit-answers");
+const output = document.querySelector("#output");
+
+const correctAnswers = ["90°", "right-angled"];
+
+function calculateScore(){
+    console.log("clicked");
+    let score = 0;
+    let index = 0;
+    const formData = new FormData(quizForm);
+    for (let value of formData.values()){
+        if(value === correctAnswers[index]){
+            score++;
+        }
+        index++;
+    }
+    output.innerText = "Your final score is: " + score; 
+}
+btnSubmitAnswers.addEventListener("click", calculateScore);
+
